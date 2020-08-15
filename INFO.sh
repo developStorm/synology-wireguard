407405e09923a6cee7728f30565f4c6bcb7a6390
Raymond Nook <59678453+developStorm@users.noreply.github.com>
2020-08-15 08:29:32 -0700

    Update INFO.sh

--- INFO.sh
+++ INFO.sh
@@ -5,7 +5,7 @@ package="WireGuard"
 version="$WIREGUARD_VERSION"
 displayname="WireGuard"
 maintainer="Andreas Runfalk"
-arch="$(pkg_get_platform)"
+arch="x64 x86 apollolake avoton braswell broadwell broadwellnk bromolow cedarview denverton dockerx64 grantley purley kvmx64 x86_64"
 description="Adds WireGuard support for your Synology NAS."
 [ "$(caller)" != "0 NULL" ] && return 0
 pkg_dump_info
